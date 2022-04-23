9b744ed38f5690c06a8de99f8c29abbd54bd2e23
jlholm <jlholmz@gmail.com>
2022-04-23 18:16:01 -0400

    all: initial commit

---
+++ lib/phi_accrual_detector.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "phi_accrual_detector/version"
+
+module PhiAccrualDetector
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/phi_accrual_detector/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module PhiAccrualDetector
+  VERSION = "0.1.0"
+end
